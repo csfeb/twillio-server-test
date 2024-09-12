c349fa33d89121c632791fbedfa49dfa35043988
Ivan Fabijanovic <ivan.fabijanovic@crowdstrike.com>
2024-09-12 15:34:44 -0700

    Refactored to express

--- server.js
+++ server.js
@@ -1,45 +1,32 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 const VoiceResponse = require('twilio').twiml.VoiceResponse;
 
 const port = process.env.PORT;
 
-function respond(body, res) {
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.post('/', (req, res) => {
   // Create TwiML response
   const twiml = new VoiceResponse();
 
-  if (body.from) {
+  if (req.body) {
     console.log('Incoming call...');
-    const caller = body.from;
-    const callerState = body.from_state;
-
     twiml.say('Hello Consumer team! This is a Twilio server.');
-    twiml.say(`You're calling from ${caller} located in ${callerState}`);
+    twiml.say(`You're calling from ${req.body.From} located in ${req.body.FromState}`);
   } else {
     twiml.say('Hello world');
   }
 
   res.writeHead(200, { 'Content-Type': 'text/xml' });
   res.end(twiml.toString());
-}
+});
 
-http
-  .createServer((req, res) => {
-    let body = [];
-    req
-      .on('data', chunk => {
-        body.push(chunk);
-      })
-      .on('end', () => {
-        body = Buffer.concat(body).toString();
-        if (body) {
-          json = JSON.parse(body);
-          console.log(json);
-          respond(json, res);
-        } else {
-          respond({}, res);
-        }
-      });
-  })
-  .listen(port, () => {
-    console.log(`Listening on port ${port}`);
-  });
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
