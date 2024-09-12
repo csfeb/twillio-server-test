63851ac2e149dccd13d83aa63a8113ded4eb5fcb
Ivan Fabijanovic <ivan.fabijanovic@crowdstrike.com>
2024-09-12 14:46:50 -0700

    Parsing some caller data

--- server.js
+++ server.js
@@ -5,10 +5,15 @@ const port = process.env.PORT;
 
 http
   .createServer((req, res) => {
+    console.log(`Incoming call with params; ${req.parameters}`);
+    const caller = req.parameters.from;
+    const callerState = req.parameters.from_state;
+
     // Create TwiML response
     const twiml = new VoiceResponse();
 
-    twiml.say('Hello Consumer team! This is Feb\'s Twilio server.');
+    twiml.say('Hello Consumer team! This is a Twilio server.');
+    twiml.say(`You're calling from ${caller} located in ${callerState}`);
 
     res.writeHead(200, { 'Content-Type': 'text/xml' });
     res.end(twiml.toString());
